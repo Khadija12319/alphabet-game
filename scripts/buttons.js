741b0923f31c3886df3a83f939914877882c518d
Khadija12319 <khadijaatul15-12319@diu.edu.bd>
2024-02-16 20:03:12 +0600

    some keyboard functions added

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,6 +1,18 @@
+function continueGame(){
+    //step 1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    //set randomly generated alphabet to the screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerHTML = alphabet;
+
+    //set background color
+    addHighlighter(alphabet);
+}
+
 function play(){
     //hide the home screen
     //show the play ground
     hideElementById('home_screen');
     showElementById('play_ground');
+    continueGame()
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,20 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function addHighlighter(elementId){
+    document.getElementById(elementId).classList.add('bg-[#FFA500]');
+}
+
+function getARandomAlphabet(){
+    //created an alphabed array
+    const alphabetString='abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    //get a random index between 0 - 25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
